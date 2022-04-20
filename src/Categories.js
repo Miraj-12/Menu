bfa5a57b45cc2754b66bc131af4b3e07ed181e7c
Miraj-12 <100298974+Miraj-12@users.noreply.github.com>
2022-04-20 20:13:58 +0600

    Add files via upload

---
+++ src/Categories.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Categories = ({ Categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {Categories.map((category, index) => {
+        return (
+          <button
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
